13e0a9935f5586465c919bebab710bb79e2bf387
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2023-05-14 22:05:59 +0530

    Set up Sanity backend

---
+++ shareme_backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'c7ddxbr4',
+    dataset: 'production'
+  }
+})
---
+++ shareme_backend/sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'shareme-jsm',
+
+  projectId: 'c7ddxbr4',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
