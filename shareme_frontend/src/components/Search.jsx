09220d6cadffaadddf4f52383cd9008b77e0281c
Imran Khan (MAQ LLC) <v-khanimr@microsoft.com>
2023-06-10 20:53:39 +0530

    Implement the Search component

--- shareme_frontend/src/components/Search.jsx
+++ shareme_frontend/src/components/Search.jsx
@@ -1,3 +1,36 @@
-export default function Search() {
-  return <div>Search</div>;
+import { useEffect, useState } from "react";
+
+import { client as sanityClient } from "../client";
+import { feedQuery, searchQuery } from "../utils/data";
+import Feed from "./Feed";
+import MasonryLayout from "./MasonryLayout";
+import Spinner from "./Spinner";
+
+export default function Search({ searchTerm, setSearchTerm }) {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const query = searchTerm
+      ? searchQuery(searchTerm.toLowerCase())
+      : feedQuery;
+
+    sanityClient.fetch(query).then((data) => {
+      setPins(data);
+      setLoading(false);
+    });
+  }, [searchTerm]);
+
+  return (
+    <div>
+      {loading && <Spinner message="Searching for pins..." />}
+
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+
+      {pins?.length === 0 && searchTerm !== "" && !loading && (
+        <div className="mt-10 text-center text-xl">No Pins found</div>
+      )}
+    </div>
+  );
 }
